be59a3a15cc1b05ff04526fb6d491ab9b0d6cb7a
dhaggard <dhaggard0@gmail.com>
2018-08-16 11:40:24 -0400

    Implementing advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,47 @@
 class User < ApplicationRecord
-    before_save { email.downcase! }
-    #VALID_NAME_REGEX = /\A[a-z]+\z/
-    validates :name, presence: true, length: { maximum: 50 }#, format: { with: VALID_NAME_REGEX }
-
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum: 255 }, 
-                      format: { with: VALID_EMAIL_REGEX }, 
-                      uniqueness: { case_sensitive: false }
-
-    has_secure_password
-
-    validates :password, presence: true, length: { minimum: 6 }
-
+  attr_accessor :remember_token
+  
+  before_save { email.downcase! }
+  #VALID_NAME_REGEX = /\A[a-z]+\z/
+  validates :name, presence: true, length: { maximum: 50 }#, format: { with: VALID_NAME_REGEX }
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, 
+  format: { with: VALID_EMAIL_REGEX }, 
+  uniqueness: { case_sensitive: false }
+  
+  has_secure_password
+  
+  validates :password, presence: true, length: { minimum: 6 }
+  
+  # another way of saying
+  # def User.digest(string) or self.digest(string) or
+  # def User.new_token or self.new_token
+  class << self
     # Returns the hash digest of the given string.
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+    
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+  
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 end
